c968c600fa90281fa543fb7834b1ffa6dd8e4c8d
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-09-27 02:51:42 +0530

    Create model.py

---
+++ pipeline/pipeline/model.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Tuple
+import warnings
+with warnings.catch_warnings():  
+     warnings.filterwarnings("ignore", category=FutureWarning)
+     import tensorflow.compat.v1 as tf
+
+class Model(metaclass=ABCMeta):
+
+      @abstractmethod
+      def __init__(self, x:tf.Tensor, y:tf.Tensor) -> None:
+          ...
+
+      @property
+      def loss(self) -> Any:
+          return self._loss
+
+      @property
+      def grad(self) -> Any:
+          return self._grad
+
+      @property
+      def evaluation_ops(self) -> List[tf.Tensor]:
+          return self._evaluation_ops
+
+      @property
+      def evaluation_metrics(self) -> List[str]:
+          return self._evaluation_metrics
+
+      @property
+      @abstractmethod
+      def input_shape(self) -> Tuple:
+          return self._input_shape
+
+      @property
+      @abstractmethod
+      def n_classes(self) -> Tuple:
+          return self._n_classes
