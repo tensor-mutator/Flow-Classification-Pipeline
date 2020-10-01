cdf1aa20b9146ac3fb5a58081fa31c3fed8b220b
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-01 22:44:34 +0530

    Update __init__.py

--- pipeline/pipeline/__init__.py
+++ pipeline/pipeline/__init__.py
@@ -2,7 +2,8 @@ __version__ = '0.1.0'
 
 from .model import Model
 from .pipeline import Pipeline
-from .metrics import MicroPrecision, MicroRecall, MicroF1Score, MacroPrecision, MacroRecall, MacroF1Score, HammingLoss
+from .metrics import (MicroPrecision, MicroRecall, MicroF1Score, MacroPrecision, MacroRecall, MacroF1Score, 
+                      HammingLoss, TP, FP, TN, FN)
 
 __all__ = ["Model", "Pipeline", "MicroPrecision", "MicroRecall", "MicroF1Score", "MacroPrecision",
-           "MacroRecall", "MacroF1Score", "HammingLoss"]
+           "MacroRecall", "MacroF1Score", "HammingLoss", "TP", "FP", "TN", "FN"]
