efb7cc51619c60d5437e8a51c60c1675d2d103bc
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-02 19:56:12 +0530

    Update pipeline.py

--- pipeline/pipeline/pipeline.py
+++ pipeline/pipeline/pipeline.py
@@ -46,7 +46,7 @@ class Pipeline:
 
       def _generate_iterator(self) -> tf.data.Iterator:
           dataset = tf.data.Dataset.from_tensor_slices((self._X_placeholder, self._y_placeholder))
-          dataset = dataset.shuffle(tf.cast(tf.shape(self._X_placeholder)[0], tf.int64)).batch(self._batch_size)
+          dataset = dataset.shuffle(tf.cast(tf.shape(self._X_placeholder)[0], tf.int64)).batch(self._batch_size).prefetch(1)
           return dataset.make_initializable_iterator()
 
       def _get_model(self, model: Model) -> Model:
