65fd8b4605611730290cbfd10489cca4e08cf50b
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-02 05:56:20 +0530

    Update metrics.py

--- pipeline/pipeline/metrics.py
+++ pipeline/pipeline/metrics.py
@@ -8,9 +8,9 @@ __all__ = ["MicroPrecision", "MicroRecall", "MicroF1Score", "MacroPrecision", "M
 
 def TP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     if type == "Macro":
-       return tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), y, tf.zeros_like(y_hat)), axis=0)
+       return tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), tf.cast(y, tf.float32), tf.zeros_like(y_hat)), axis=0)
     else:
-       return tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), y, tf.zeros_like(y_hat)))
+       return tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), tf.cast(y, tf.float32), tf.zeros_like(y_hat)))
 
 def FP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     if type == "Macro":
@@ -30,20 +30,20 @@ def TN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
 
 def FN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     if type == "Macro":
-       return tf.reduce_sum(tf.where(tf.equal(y_hat, 0), y,
+       return tf.reduce_sum(tf.where(tf.equal(y_hat, 0), tf.cast(y, tf.float32),
                                      tf.zeros_like(y_hat)), axis=0)
     else:
-       return tf.reduce_sum(tf.where(tf.equal(y_hat, 0), y,
+       return tf.reduce_sum(tf.where(tf.equal(y_hat, 0), tf.cast(y, tf.float32),
                                      tf.zeros_like(y_hat)))
 
 def MicroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     TP_plus_FP = tf.reduce_sum(y_hat)
-    TP = tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), y, tf.zeros_like(y_hat)))
+    TP = tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), tf.cast(y, tf.float32), tf.zeros_like(y_hat)))
     return TP/TP_plus_FP
 
 def MicroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     TP_plus_FN = tf.reduce_sum(y)
-    TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_hat, tf.zeros_like(y)))
+    TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_hat, tf.cast(tf.zeros_like(y), tf.float32)))
     return TP/TP_plus_FN
 
 def MicroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
@@ -53,12 +53,12 @@ def MicroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
 
 def MacroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     TP_plus_FP = tf.reduce_sum(y_hat, axis=0)
-    TP = tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), y, tf.zeros_like(y_hat)), axis=0)
+    TP = tf.reduce_sum(tf.where(tf.cast(y_hat, tf.bool), tf.cast(y, tf.float32), tf.zeros_like(y_hat)), axis=0)
     return tf.reduce_mean(TP/TP_plus_FP)
 
 def MacroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     TP_plus_FN = tf.reduce_sum(y, axis=0)
-    TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_hat, tf.zeros_like(y)), axis=0)
+    TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_hat, tf.cast(tf.zeros_like(y), tf.float32)), axis=0)
     return tf.reduce_mean(TP/TP_plus_FN)
 
 def MacroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
@@ -67,4 +67,4 @@ def MacroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     return 2*MacroPR*MacroRC/(MacroPR+MacroRC)
 
 def HammingLoss(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
-    return tf.cast(tf.equal(y, y_hat), tf.float32)/tf.reduce_sum(tf.ones_like(y_hat))
+    return tf.cast(tf.equal(tf.cast(y, tf.float32), y_hat), tf.float32)/tf.reduce_sum(tf.ones_like(y_hat))
