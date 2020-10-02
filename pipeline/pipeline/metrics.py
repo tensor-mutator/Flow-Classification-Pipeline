a3759ef22a9843ce188ca2c2f7a97986d8941821
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-02 08:20:56 +0530

    Update metrics.py

--- pipeline/pipeline/metrics.py
+++ pipeline/pipeline/metrics.py
@@ -8,7 +8,7 @@ __all__ = ["MicroPrecision", "MicroRecall", "MicroF1Score", "MacroPrecision", "M
 
 def TP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     if type == "Macro":
        return tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), y, tf.zeros_like(y_cap)), axis=0)
     else:
@@ -16,7 +16,7 @@ def TP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
 
 def FP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     if type == "Macro":
        return tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), tf.equal(y, 0), tf.zeros_like(y_cap)), axis=0)
     else:
@@ -24,7 +24,7 @@ def FP(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
 
 def TN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     if type == "Macro":
        return tf.reduce_sum(tf.where(tf.equal(y_cap, 0), tf.equal(y, 0), tf.zeros_like(y_cap)), axis=0)
     else:
@@ -32,7 +32,7 @@ def TN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
 
 def FN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     if type == "Macro":
        return tf.reduce_sum(tf.where(tf.equal(y_cap, 0), y, tf.zeros_like(y_cap)), axis=0)
     else:
@@ -40,14 +40,14 @@ def FN(y: tf.Tensor, y_hat: tf.Tensor, type: str = "Macro") -> tf.Tensor:
 
 def MicroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     TP_plus_FP = tf.reduce_sum(y_cap)
     TP = tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), y, tf.zeros_like(y_cap)))
     return TP/TP_plus_FP
 
 def MicroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     TP_plus_FN = tf.reduce_sum(y)
     TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_cap, tf.zeros_like(y)))
     return TP/TP_plus_FN
@@ -59,14 +59,14 @@ def MicroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
 
 def MacroPrecision(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     TP_plus_FP = tf.reduce_sum(y_cap, axis=0)
     TP = tf.reduce_sum(tf.where(tf.cast(y_cap, tf.bool), y, tf.zeros_like(y_cap)), axis=0)
     return tf.reduce_mean(TP/TP_plus_FP)
 
 def MacroRecall(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     TP_plus_FN = tf.reduce_sum(y, axis=0)
     TP = tf.reduce_sum(tf.where(tf.cast(y, tf.bool), y_cap, tf.zeros_like(y)), axis=0)
     return tf.reduce_mean(TP/TP_plus_FN)
@@ -78,5 +78,5 @@ def MacroF1Score(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
 
 def HammingLoss(y: tf.Tensor, y_hat: tf.Tensor) -> tf.Tensor:
     y_pred = tf.argmax(y_hat, axis=-1)
-    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1])
+    y_cap = tf.one_hot(indices=y_pred, depth=tf.shape(y)[-1], dtype=tf.int32)
     return tf.reduce_sum(tf.equal(y, y_cap))/tf.reduce_sum(tf.ones_like(y_cap))
