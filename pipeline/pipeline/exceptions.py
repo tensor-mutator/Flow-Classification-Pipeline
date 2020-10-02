fe3c9af51026da5cfdd1ac6d06f5861f1d26b783
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-02 07:31:56 +0530

    Update exceptions.py

--- pipeline/pipeline/exceptions.py
+++ pipeline/pipeline/exceptions.py
@@ -3,14 +3,14 @@ __all__ = ["InvalidLossError", "InvalidOptimizerError", "InvalidMetricError"]
 class InvalidLossError(Exception):
 
       def __init__(self, msg: str) -> None:
-          super(Pipeline.InvalidLossError, self).__init__(msg)
+          super(InvalidLossError, self).__init__(msg)
 
 class InvalidOptimizerError(Exception):
 
       def __init__(self, msg: str) -> None:
-          super(Pipeline.InvalidOptimizerError, self).__init__(msg)
+          super(InvalidOptimizerError, self).__init__(msg)
 
 class InvalidMetricError(Exception):
 
       def __init__(self, msg: str) -> None:
-          super(Pipeline.InvalidMetricError, self).__init__(msg)
+          super(InvalidMetricError, self).__init__(msg)
