4d39359be651b86be3abfbd7d3f5b40463866a30
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-09 19:37:31 +0530

    Update __init__.py

--- pipeline/pipeline/__init__.py
+++ pipeline/pipeline/__init__.py
@@ -7,6 +7,8 @@ with warnings.catch_warnings():
 tf.logging.set_verbosity(tf.logging.WARN)
 tf.logging.set_verbosity(tf.logging.ERROR)
 tf.disable_eager_execution()
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
 from .model import Model
 from .pipeline import Pipeline
 from .metrics import (MicroPrecision, MicroRecall, MicroF1Score, MacroPrecision, MacroRecall, MacroF1Score, 
